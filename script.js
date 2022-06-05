3ae0e7623995dc341098a190ec1547564a3c5b04
Bhakti <ghaghdabhakti@gmail.com>
2022-06-05 13:11:59 +0400

    Add function to choose computer's play

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello, world!');
+//randomly chooses rock, paper or scissors
+function computerPlay() {
+    let options = [
+        'rock',
+        'paper',
+        'scissors'
+    ];
+    let randomNumber = Math.floor(Math.random()*options.length);
+
+    return options[randomNumber];
+}
+
+console.log('The computer played ' + computerPlay());
+
+
