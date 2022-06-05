e0c8975065b003586f9aa2cb3e8e50d92c4a7dd3
Bhakti <ghaghdabhakti@gmail.com>
2022-06-05 13:15:55 +0400

    Add function to determine winner

--- script.js
+++ script.js
@@ -10,10 +10,41 @@ function computerPlay() {
     return options[randomNumber];
 }
 
+//prompt user to choose rock, paper or scissors
 function playerPlay() {
     const input = prompt('Select rock paper or scissors');
     return input;
 }
 
-console.log('The computer played ' + computerPlay());
-console.log('The player played ' + playerPlay());
+//compare the parameters to decide winner
+function gameRound(playerSelection, computerSelection) {
+    let points = 0;
+    if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors' ||
+    playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock' ||
+    playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        points = 1;
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock' ||
+    playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper' ||
+    playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors') {
+        points = 0;
+    }
+    else
+        points = -1;
+
+    return points;
+}
+
+const playerSelection = playerPlay();
+const computerSelection = computerPlay(); 
+console.log('You played ' + playerSelection);
+console.log('Computer played ' + computerSelection);
+const result = gameRound(playerSelection, computerSelection);
+if(result == 1) {
+    console.log('Player wins!');
+}
+else if(result == -1) {
+    console.log('Computer wins!');
+}
+else if(result == 0) {
+    console.log('Draw.');
